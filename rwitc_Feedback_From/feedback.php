515dc392f56110a3b3cf538b1f4ea3cc2d96bc51
klopes29 <brensa30@gmail.com>
2017-01-20 12:57:03 +0530

    initialCommit

---
+++ rwitc_Feedback_From/feedback.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+
+<head> <title>Home - RWITC</title>
+ <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+
+<article>
+  <header align="center">
+  	<img src="logo.jpeg" alt="logo" />
+    <h1>ROYAL WESTERN INDIA TURF CLUB, LTD.</h1>
+    <p>Race Course, Mahalakshmi, Mumbai – 400034.</p>
+    <p>E-mail : secretary@rwitc.com, Website – <a href="http://www.rwitc.com">www.rwitc.com</a></p>
+  </header>
+ 
+</article>
+
+<h1 align="center"><u>Feedback Form</u></h1><br>
+
+<p class="p" align="center">Thank you for organising your function / event at the Mumbai Race Course.We would request you to kindly spare few minutes to give your very valuable feedback which will go a long way in further improvements.<i>(Please tick “&#x2714;” the appropriate boxes).</i> </p>
+<br/>
+
+<Form action="test.php" id="" Method="POST">
+
+1.	Where was your function / event held ?
+<br><br>
+<input type="checkbox" name="event[]" value="Members Pavillion"> Members Pavillion <br>
+<input type="checkbox" name="event[]" value="First Enclosure Lawn"> First Enclosure Lawn<br>
+<input type="checkbox" name="event[]" value="Second Enclosure Lawn"> Second Enclosure Lawn (Far Pavillion)
+<br><br>
+
+2.	When was your function / event held ?
+<br><br>
+Function : <input type="text" name="name" placeholder="Function Name" required> </input>&nbsp;
+Event Date:<input type="date" name="date"  required > </input>
+<br><br>
+
+3.	How did you hear about our venue?
+<br><br>
+<input type="radio" name="venue" value="Member of the Club"> Member of the Club <br>
+<input type="radio" name="venue" value="Previous Event Attendee"> Previous Event Attendee<br>
+<input type="radio" name="venue" value="Friends / Relatives"> Friends / Relatives<br>
+<input type="radio" name="venue" value="Repeat Guest"> Repeat Guest<br>
+<input type="radio" name="venue" value="Clubs Website"> Club’s Website<br>
+<input type="radio" name="venue" value="Walk-in"> Walk-in<br>
+<input type="radio" name="venue" value="Others"> Others – Please Specify :<br>
+<textarea  name="venue_txtarea"></textarea>
+<br><br>
+
+4.	You chose this venue because of
+<br><br>
+<input type="radio" name="venuereason" value="Brand Image"> Brand Image <br>
+<input type="radio" name="venuereason" value="Value for money"> Value for money<br>
+<input type="radio" name="venuereason" value="Location / Proximity"> Location / Proximity<br>
+<input type="radio" name="venuereason" value="Infrastructure / Facilities / Ambience"> Infrastructure / Facilities / Ambience<br>
+<input type="radio" name="venuereason" value="Recommendations"> Recommendations<br>
+<input type="radio" name="venuereason" value="Others"> Other – Please Specify<br>
+<textarea  name="venuereason_txtarea"></textarea>
+<br><br>
+
+5.	Please rate the following<br><br>Staff Courtesy / helpful nature
+<br><br>
+<input type="radio" name="rate" value="Excellent"> Excellent <br>
+<input type="radio" name="rate" value="Good"> Good<br>
+<input type="radio" name="rate" value="Average"> Average<br>
+<input type="radio" name="rate" value="Below Average"> Below Average<br>
+<input type="radio" name="rate" value="Poor"> Poor<br>
+<input type="radio" name="rate" value="Not applicable"> Not applicable<br>
+<br><br>
+
+6.	Booking process
+<br><br>
+<input type="radio" name="booking" value="Excellent"> Excellent <br>
+<input type="radio" name="booking" value="Good"> Good<br>
+<input type="radio" name="booking" value="Average"> Average<br>
+<input type="radio" name="booking" value="Below Average"> Below Average<br>
+<input type="radio" name="booking" value="Poor"> Poor<br>
+<input type="radio" name="booking" value="Not applicable"> Not applicable<br>
+<br><br>
+
+7.	Change Rooms ( if used)
+<br><br>
+<input type="radio" name="changeroom" value="Excellent"> Excellent <br>
+<input type="radio" name="changeroom" value="Good"> Good<br>
+<input type="radio" name="changeroom" value="Average"> Average<br>
+<input type="radio" name="changeroom" value="Below Average"> Below Average<br>
+<input type="radio" name="changeroom" value="Poor"> Poor<br>
+<input type="radio" name="changeroom" value="Not applicable"> Not applicable<br>
+<br><br>
+
+8.	Rest Rooms
+<br><br>
+<input type="radio" name="rstroom" value="Excellent"> Excellent <br>
+<input type="radio" name="rstroom" value="Good"> Good<br>
+<input type="radio" name="rstroom" value="Average"> Average<br>
+<input type="radio" name="rstroom" value="Below Average"> Below Average<br>
+<input type="radio" name="rstroom" value="Poor"> Poor<br>
+<input type="radio" name="rstroom" value="Not applicable"> Not applicable<br>
+<br><br>
+
+9.	Lawns (in the First / Second Enclosure, (if used)
+<br><br>
+<input type="radio" name="lawn" value="Excellent"> Excellent <br>
+<input type="radio" name="lawn" value="Good"> Good<br>
+<input type="radio" name="lawn" value="Average"> Average<br>
+<input type="radio" name="lawn" value="Below Average"> Below Average<br>
+<input type="radio" name="lawn" value="Poor"> Poor<br>
+<input type="radio" name="lawn" value="Not applicable"> Not applicable<br>
+<br><br>
+
+10.	Cleanliness / Hygiene
+<br><br>
+<input type="radio" name="clean" value="Excellent"> Excellent <br>
+<input type="radio" name="clean" value="Good"> Good<br>
+<input type="radio" name="clean" value="Average"> Average<br>
+<input type="radio" name="clean" value="Below Average"> Below Average<br>
+<input type="radio" name="clean" value="Poor"> Poor<br>
+<input type="radio" name="clean" value="Not applicable"> Not applicable<br>
+<br><br>
+
+11.	Billing Process (Prompt & Accuracy)
+<br><br>
+<input type="radio" name="bill" value="Excellent"> Excellent <br>
+<input type="radio" name="bill" value="Good"> Good<br>
+<input type="radio" name="bill" value="Average"> Average<br>
+<input type="radio" name="bill" value="Below Average"> Below Average<br>
+<input type="radio" name="bill" value="Poor"> Poor<br>
+<input type="radio" name="bill" value="Not applicable"> Not applicable<br>
+<br><br>
+
+12.	Overall experience
+<br><br>
+<input type="radio" name="exp" value="Excellent"> Excellent <br>
+<input type="radio" name="exp" value="Good"> Good<br>
+<input type="radio" name="exp" value="Average"> Average<br>
+<input type="radio" name="exp" value="Below Average"> Below Average<br>
+<input type="radio" name="exp" value="Poor"> Poor<br>
+<br><br>
+
+13.	Would you recommend the Club to your contacts ?
+<br><br>
+<input type="radio" name="recommend" value="YES"> YES <br>
+<input type="radio" name="recommend" value="NO"> NO <br>
+<br><br>
+
+14.	Any other comments & suggestions 
+<br><br>
+<textarea  name="comments"></textarea>
+<br><br>
+
+15.	Contact Details :
+<br><br>
+Name :<br><input type="text" name="cust_name" placeholder="Name" required> </input><br>
+E-mail :<br><input type="email" name="email" placeholder="Email" required> </input><br>
+Phone :<br><input type="number" name="phone" placeholder="+91" required> </input><br>
+Address :<br>
+<textarea  name="address" placeholder="Address" required></textarea>
+<br><br>
+
+16.	Can we use your feedback for promotional reason ?
+<br><br>
+<input type="radio" name="pro" value="YES"> YES <br>
+<input type="radio" name="pro" value="NO"> NO <br>
+<br>
+
+<p><b>NOTE :</b> Your  feedback will be kept as confidential.</p>
+
+<input type="Submit"> </input> &nbsp;<button type="reset" value="Reset">Reset</button>
+</Form>
+<br>
+
+</html>
